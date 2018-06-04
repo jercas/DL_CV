db1fec6972a188ce7396afdc9755b660a8f1dbb8
JerCas Ety <jercas0618@163.com>
2018-06-04 14:24:35 +0800

    Implementing the config file of the project 'tiny-imagenet classification' and a util script to extract data from the raw images to constructing a HDF5 file.

---
+++ selfImplementation/SB/application/tiny_imagenet/build_hdf5_dataset.py
@@ -0,0 +1,85 @@
+"""
+Created on Mon Jun 3 10:37:00 2018
+
+@author: jercas
+"""
+
+import tiny_imagenet_config as config
+# Using the LabelEncoder to encode the WordNet IDs as single integers.
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from io.hdf5DatasetWriter import HDF5DatasetWriter
+from imutils import paths
+import numpy as np
+import progressbar
+import json
+import cv2
+import os
+
+# Grab the paths to the training images, then extract the training class labels and encode them.
+trainPaths = list(paths.list_images(config.TRAIN_PATH))
+trainLabels = [path.split(os.path.sep)[-3] for path in trainPaths]
+le = LabelEncoder()
+trainLabels = le.fit_transform(trainLabels)
+
+# Performing the stratified sampling from the training set to construct a testing set.
+(trainPaths, testPaths, trainLabels, testLabels) = train_test_split(trainPaths, trainLabels,
+                                                                    test_size=config.NUM_TEST_IMAGES,
+                                                                    stratify=trainLabels, random_state=42)
+
+# Load the validation filename => class from the file and then use these mappings to build the validation paths and labels list.
+mappings  = open(config.VAL_MAPPINGS).read().strip().split("\n")
+# Extract the top two columns, the image filename and the WordNet ID, in each row.
+mappings  = [nameToClass.split("\t")[:2] for nameToClass in mappings]
+# Extract the path and class label of each validation example from the mapping file.
+valPaths  = [os.path.sep.join([config.VAL_PATH, mapping[0]]) for mapping in mappings]
+# Transform the WordNet ID string to a unique class label integer by looping over the WordNet IDs in each row and applying
+#the label encoder.
+valLabels = le.transform([mapping[1] for mapping in mappings])
+
+# Construct a list pairing the training, validation, and testing image paths along with their corresponding labels and
+#output HDF5 files.
+dataset = [
+	("train", trainPaths, trainLabels, config.TRAIN_HDF5),
+	("val",   valPaths,   valLabels,   config.VAL_HDF5),
+	("test",  testPaths,  testLabels,  config.TEST_HDF5)]
+
+# Initialize the lists of RGB channel average.
+(R, G, B) = ([], [], [])
+
+# Loop over the dataset tuple.
+for (nameOfHDF5, paths, labels, outputPath) in dataset:
+	# Create the HDF5 writer.
+	print("[INFO] building {}...".format(outputPath))
+	writer = HDF5DatasetWriter((len(paths), 64, 64, 3), outputPath)
+
+	# Initialize the progress bar.nameOfHDF5
+	widgets = ["Building Dataset: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
+	proBar  = progressbar.ProgressBar(maxval=len(paths), widgets=widgets)
+	proBar.start()
+
+	# Loop over each image path.
+	for (i, (path, label)) in enumerate(zip(paths, labels)):
+		# Load the image from disk.
+		image = cv2.imread(path)
+
+		# If we are building the training dataset, then compute the mean of each channel in the image, then update the
+		#respective lists.
+		if nameOfHDF5 == "train":
+			(b, g, r) = cv2.mean(image)[:3]
+			R.append(r)
+			G.append(g)
+			B.append(b)
+
+		# Add the image and its label to the HFD5 dataset.
+		writer.add([image], [label])
+		proBar.update()
+	proBar.finish()
+	writer.close()
+
+# Construct a dictionary of averages, then serialize the means to a JSON file.
+print("[INFO] serialize the RGB mean...")
+Dictionary = {"R":np.mean(R), "G":np.mean(G), "B":np.mean(B)}
+f = open(config.DATASET_MEAN, "w")
+f.write(json.dumps(Dictionary))
+f.close()
---
+++ selfImplementation/SB/application/tiny_imagenet/config/tiny_imagenet_config.py
@@ -0,0 +1,38 @@
+"""
+Created on Mon Jun 3 10:06:00 2018
+
+@author: jercas
+"""
+
+from os import path
+
+# Define the path to the training and validation directories.
+TRAIN_PATH = "../dataset/train"
+VAL_PATH   = "../dataset/val/images"
+
+# Define the path to the file that map validation filenames to their corresponding class labels(i.e., WordNet-IDs).
+VAL_MAPPINGS = "../dataset/val/val_annotations.txt"
+
+# Define the paths to the WordNet hierarchy files which are used to generate class labels from the WordNet-IDs.
+WORDNET_IDS = "../dataset/wnids.txt"
+WORD_LABELS = "../dataset/words.txt"
+
+# Since there is no access to the testing labels, we need to take a portion of images from the training data and use it instead.
+NUM_CLASSES = 200
+# 50 images per class(for a total of 10,000 images).
+NUM_TEST_IMAGES = 50 * NUM_CLASSES
+
+# Define the path to the output training, validation and testing HDF5 files.
+TRAIN_HDF5 = "./dataset/hdf5/train.hdf5"
+VAL_HDF5   = "./dataset/hdf5/val.hdf5"
+TEST_HDF5  = "./dataset/hdf5/test.hdf5"
+
+# Define the path to the dataset mean which enable to perform the mean normalization.
+DATASET_MEAN = "./model/tiny_imagenet_mean.json"
+
+# Define the path to the output directory used for storing plots, classification reports, models, etc.
+OUTPUT_DIR = "./output"
+MODEL_DIR  = "./model"
+MODEL_PATH   = path.sep.join([MODEL_DIR, "checkpoints/deeperGoogLeNet_tinyImageNet.hdf5"])
+FIGURE_PATH  = path.sep.join([OUTPUT_DIR, "deeperGoogLeNet_tinyImageNet.png"])
+JSON_PATH    = path.sep.join([OUTPUT_DIR, "deeperGoogLeNet_tinyImageNet.json"])
